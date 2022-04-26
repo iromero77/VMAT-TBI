81b3974b35f11538dac2bf831a54c243b90b5bf4
esimiele <simiele77@gmail.com>
2022-04-26 09:10:35 -0400

    Initial upload of revised code

---
+++ ESAPI_MT/VMATTBIAutoPlanMT/VMATTBIAutoPlanMT/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VMATTBIAutoPlanMT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    MainWindow mw = new MainWindow(e.Args);
+        //    mw.Show();
+        //}
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var args = e.Args;
+            if (args != null && args.Count() > 0)
+            {
+                //foreach (var arg in args)
+                //{
+                //    // write code to use the command line arg value 
+                //}
+            }
+            MainWindow mw = new MainWindow(e.Args);
+            mw.Show();
+        }
+    }
+}
---
+++ ESAPI_MT/VMATTBIAutoPlanMT/launchVMATTBIAutoPlan/Script.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context)
+        {
+            try
+            {
+                string path = AppExePath();
+                if (!string.IsNullOrEmpty(path))
+                {
+                    ProcessStartInfo p = new ProcessStartInfo(path);
+                    string configFile = "";
+                    if (File.Exists(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\configuration\\VMAT_TBI_config.ini")) configFile = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\configuration\\VMAT_TBI_config.ini";
+                    if (context.Patient != null) p.Arguments = String.Format("{0} {1} {2}",context.Patient.Id,context.StructureSet.Id,configFile);
+                    Process.Start(p);
+                }
+                else MessageBox.Show("Error! VMATTBIAutoPlan executable NOT found!");
+            }
+            catch (Exception e) { MessageBox.Show(e.Message); };
+        }
+        private string AppExePath()
+        {
+            return FirstExePathIn(AssemblyDirectory());
+        }
+
+        private string FirstExePathIn(string dir)
+        {
+            return Directory.GetFiles(dir, "*.exe").FirstOrDefault(x => x.Contains("VMATTBIAutoPlanMT"));
+        }
+
+        private string AssemblyDirectory()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+    }
+}
